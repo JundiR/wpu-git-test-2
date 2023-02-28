c3b9fb471b385d6046e9eac7755ffc688b22924d
JundiR <jundi17fr@gmail.com>
2023-02-28 08:00:01 +0700

    Menambahkan list di index.php

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, consequuntur quia voluptatibus quas vitae deserunt? Libero excepturi vitae aliquam enim natus ducimus molestias nulla, accusamus cum? Nisi velit ullam reiciendis!</p>
     <p>Paragraf baru</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
